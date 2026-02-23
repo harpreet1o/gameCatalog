7efaf154f4d984441838ebbc3aa117506b79a053
Harpreet <harpreetx01@outlook.com>
2026-02-22 17:32:38 -0800

    added the async to keep the thread moving and also added the repository to prevent the controller accessing the db directly

---
+++ GamecatlogAPI/GamecatlogAPI/Repositores/IGamerepository.cs
@@ -0,0 +1,14 @@
+﻿using GamecatalogAPI.Models.Domain;
+namespace GamecatalogAPI.Repositores
+{
+    public interface IGamerepository
+    {
+        Task<List<Game>> GetAllAsync();
+
+        Task<Game?>GetByIdAsync(Guid id);
+
+        Task<Game?> CreateAsync(Game game);
+
+
+    }
+}
---
+++ GamecatlogAPI/GamecatlogAPI/Repositores/SQLGameRepository.cs
@@ -0,0 +1,19 @@
+﻿using GamecatalogAPI.Data;
+using GamecatalogAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamecatalogAPI.Repositores
+{
+    public class SQLGameRepository : IGamerepository
+    {
+        private readonly GamesDBContext dbContext;
+        public SQLGameRepository(GamesDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<List<Game>> GetAllAsync()
+        {
+          return await dbContext.Game.ToListAsync();
+        }
+    }
+}
