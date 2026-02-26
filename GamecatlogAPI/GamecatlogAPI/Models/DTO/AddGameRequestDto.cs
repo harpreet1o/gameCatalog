d8f61b428a4123b8b0535abf83f0f17acd495a3b
Harpreet <harpreetx01@outlook.com>
2026-02-25 20:40:06 -0800

    improved the data types and restriction added to data in DTOs and model entered to protect db

--- GamecatlogAPI/GamecatlogAPI/Models/DTO/AddGameRequestDto.cs
+++ GamecatlogAPI/GamecatlogAPI/Models/DTO/AddGameRequestDto.cs
@@ -1,9 +1,20 @@
-﻿namespace GamecatalogAPI.Models.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamecatalogAPI.Models.DTO;
 
 public record AddGameRequestDto(
+    [Required(ErrorMessage = "Name is required and cannot be empty")]
+    [MinLength(2, ErrorMessage = "Name must be at least 2 characters")]
     string Name,
+
+    [Required(ErrorMessage = "Description is required")]
     string Description,
+
+    [Range(0.01, 10000.00, ErrorMessage = "Price must be between 0.01 and 10,000")]
     decimal Price,
+
+    [Required(ErrorMessage = "Genre is required")]
     string Genre,
+    [Url(ErrorMessage = "Invalid URL format")]
     string? GameImageURL
 );
--- GamecatlogAPI/GamecatlogAPI/Models/DTO/UpdateGameRequestDto.cs
+++ GamecatlogAPI/GamecatlogAPI/Models/DTO/UpdateGameRequestDto.cs
@@ -1,9 +1,21 @@
-﻿namespace GamecatalogAPI.Models.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamecatalogAPI.Models.DTO;
 
 public record UpdateGameRequestDto(
-   string Name,
-   string Description,
-   decimal Price,
-   string Genre,
-   string? GameImageURL
+
+[Required(ErrorMessage = "Name is required")]
+    string Name,
+
+    [Required(ErrorMessage = "Description is required")]
+    string Description,
+
+    [Range(0.01, 1000000.00, ErrorMessage = "Price must be positive")]
+    decimal Price,
+
+    [Required(ErrorMessage = "Genre is required")]
+    string Genre,
+
+    [Url(ErrorMessage = "Invalid URL format")]
+    string? GameImageURL
  );
